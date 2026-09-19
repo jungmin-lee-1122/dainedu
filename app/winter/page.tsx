2b1eee2f2d60d644843ac7cfe174b8ac0909f226
jungminlee <burningtree02@gmail.com>
2026-09-19 18:13:04 +0900

    윈터 강사진: 느낌표 제거, 골드 스윕 속도 완화

--- app/winter/page.tsx
+++ app/winter/page.tsx
@@ -232,7 +232,7 @@ export default function WinterPage() {
             <span className="wt-fc-shine" aria-hidden="true" />
             <span className="wt-fc-punch-rule" aria-hidden="true" />
             <p className="wt-fc-punch">
-              전과목 <em>학원가 대표 강사</em> 출강<i>!</i>
+              전과목 <em>학원가 대표 강사</em> 출강
             </p>
           </div>
         </div>
