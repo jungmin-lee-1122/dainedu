9805ec79a425cfae6d1dd96e7f085daf67540ee4
jungminlee <burningtree02@gmail.com>
2026-09-22 18:00:02 +0900

    메인 팝업 임시 비활성화 (Cloudflare 팝업과 중복 방지)

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,32 @@ import { landingScript } from "./landingScript";
 import { winterPopupMarkup } from "./winterPopup";
 import { winterPopupScript } from "./winterPopupScript";
 
+/** 윈터스쿨 팝업 표시 여부
+ *  현재 Cloudflare 쪽에서 팝업을 따로 띄우고 있어 겹치지 않도록 꺼둡니다.
+ *  Cloudflare 팝업을 걷어내면 true 로 바꾸면 다시 나옵니다.
+ */
+const SHOW_WINTER_POPUP = false;
+
 export default function Home() {
   return (
     <>
       <div dangerouslySetInnerHTML={{ __html: landingMarkup }} />
-      <div dangerouslySetInnerHTML={{ __html: winterPopupMarkup }} />
       <Script
         id="dain-landing"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{ __html: landingScript }}
       />
-      <Script
-        id="dain-winter-popup"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{ __html: winterPopupScript }}
-      />
+
+      {SHOW_WINTER_POPUP && (
+        <>
+          <div dangerouslySetInnerHTML={{ __html: winterPopupMarkup }} />
+          <Script
+            id="dain-winter-popup"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{ __html: winterPopupScript }}
+          />
+        </>
+      )}
     </>
   );
 }
